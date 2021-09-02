9beb55a1e3ccea4d7ff31366b4690a81b27137a0
JW Kim <kl529@naver.com>
2021-09-02 15:45:35 +0900

    [김지원] 로직변경

--- solution/BOJ_1149_RGB거리/김지원.java
+++ solution/BOJ_1149_RGB거리/김지원.java
@@ -1,34 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
+
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static int N;
+    static int[][] arr;
+    static int[][] dp;
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
+        //1. 데이터 입력받기
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][3];
+        dp = new int[N][3];
 
-        int[][] Cost = new int[N][3];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-
-            Cost[i][0] = Integer.parseInt(st.nextToken());
-            Cost[i][1] = Integer.parseInt(st.nextToken());
-            Cost[i][2] = Integer.parseInt(st.nextToken());
-
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        // 1부터 N-1까지 각 i별 i-1의 서로 다른 색상 중 최솟값을 누적하여 더한다.
-        for (int i = 1; i < N; i++) {
-            Cost[i][0] += Math.min(Cost[i - 1][1], Cost[i - 1][2]);
-            Cost[i][1] += Math.min(Cost[i - 1][0], Cost[i - 1][2]);
-            Cost[i][2] += Math.min(Cost[i - 1][0], Cost[i - 1][1]);
+        //2. 점화식으로 풀기
+        for(int i=0; i<3; i++){
+            dp[0][i] = arr[0][i];
         }
 
-        System.out.println(Math.min(Math.min(Cost[N - 1][0], Cost[N - 1][1]), Cost[N - 1][1]));
+        for(int i=1; i<N; i++){
+            dp[i][0] = Math.min(dp[i-1][1],dp[i-1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i-1][0],dp[i-1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i-1][1],dp[i-1][0]) + arr[i][2];
+        }
+
+        System.out.println(Math.min(Math.min(dp[N-1][0], dp[N-1][1]), dp[N-1][2]));
     }
 }
