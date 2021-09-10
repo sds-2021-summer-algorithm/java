7ec06ef5c9b1a86151fe2803533069eb9bd40be3
yukyeongmin <fibikong@naver.com>
2021-09-10 17:14:52 +0900

    [유경민]인구인동 (다른 의미로 엄청난 코드를 짜고 말았습니다... 4000ms를 넘네요...)

--- solution/BOJ_16234_인구-이동/ykm.java
+++ solution/BOJ_16234_인구-이동/ykm.java
@@ -1,30 +1,19 @@
-package P16234; //유니온 파인드?
+package P16234; //구현 + dfs + 많은 배열 활용  -> 은서님 코드 참고
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int N; // 폭
     static int num; // 실제 나라수
-    static int[] country;
-    static boolean[][] neighbor; // 각 나라별 국경 오픈 여부
+    static int[][] country; // 나라별 인구수
+    static boolean[][] visited; // 방문 여부
+    static boolean[][] isUnion; // 연합국 표시
+    static boolean flag = false;
+
+    // 국경열때 count
     static int max;
     static int min;
-
-    static class Road{
-        int cityNum;
-        boolean isOpen = false;
-
-        @Override
-        public String toString() {
-            return "Road [cityNum=" + cityNum + ", isOpen=" + isOpen + "]";
-        }
-
-        public Road(int cityNum) {
-            this.cityNum = cityNum;
-        }
-    }
-
 
     public static void main(String[] arg) throws IOException{
         System.setIn(new FileInputStream("P16234/input.txt"));
@@ -35,92 +24,96 @@ public class Main {
         num = (int)Math.pow(N,2);
         min = Integer.parseInt(st.nextToken());
         max = Integer.parseInt(st.nextToken());
-        country = new int[num+1];
-        neighbor = new boolean [num+1][num+1]; // 이웃표시
+        country = new int[N+1][N+1];
 
         for(int i = 1 ; i<=N ; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 1 ; j<=N ; j++){
-                int current = (i-1) * N + j;
-                country[current] = Integer.parseInt(st.nextToken());
+                country[i][j] = Integer.parseInt(st.nextToken()); // 나라별 인구수
             } 
         }
 
 
         int count = 0;
-        resetOpen();
+        
+
         while(true){
-            if(!check()) continue;
+            flag = false;                               // 국경을 여는 나라가 있는지 확인
+            visited = new boolean[N+1][N+1];            // 방문여부 표시
+            
+                     
+            for(int i = 1 ; i<=N ; i++){
+                for(int j = 1; j<=N; j++){
+    
+                    if(visited[i][j]) continue; // 아직 연합국에 속하지 않으면 
+    
+                    isUnion = new boolean[N+1][N+1];             // i,j와 연합을 이루는지 확인
+
+                    int [] ans = visit(i,j,0,0);                 //dfs
+                    Union(ans[0],ans[1]);
+                }
+            }
+            if(!flag) break;
             count++;
-            makeAvg();
         }
 
-        System.out.print(count);
+        System.out.println(count);
+        br.close();
     }
 
-    // 인구 평균
-    public static void makeAvg(){
-        for(int i =1 ; i<=num ; i++){
-            int sum = country[i];
-            int count = 1;
-            for(int j = 1 ; j<=num; j++){
-                if(neighbor[i][j]){
-                    sum += country[j];
-                    count++;
+    // 연합인 나라수와, 인구합을 출력
+    static int[] visit(int i, int j, int union, int sum){ //i,j와 연합인 나라수, 연합국 인구합
+        int [] ans = {union, sum};             // 리턴용
+        int [] mx = {-1,1,0,0}; // 상, 하, 좌, 우
+        int [] my = {0,0,-1,1};
+        sum += country[i][j];
+        union++;
+        visited[i][j] = true;
+        isUnion[i][j] = true;
+        for(int k = 0 ; k<4; k++){
+
+            int nextX = i+mx[k];
+            int nextY = j+my[k];
+
+            if(nextX<1 || nextY<1 ||nextX>N ||nextY>N) continue;    // 범위밖
+
+
+            if(!visited[nextX][nextY] && !isUnion[nextX][nextY]){ // 아직 어느 연합에 포함되지 않은 나라를 대상으로
+                int diff = Math.abs(country[i][j] - country[nextX][nextY]);
+
+                if(diff>=min && diff<= max){
+                    // (i,j)와 (nextX,nextY)는 같은 연합
+                    // sum += country[nextX][nextY];
+                    // union++;
+                    visited[nextX][nextY] = true;
+                    int[] ret = visit(nextX,nextY,union,sum);
+                    if(union!=ret[0]){
+                        isUnion[nextX][nextY] = true;
+                    }
+                    union = ret[0];
+                    sum = ret[1];
+                    flag = true;                    
                 }
             }
-            sum /= count;
-            country[i] = sum;
-            for(int j = 1 ; j<=num; j++){
-                if(neighbor[i][j]){
-                    country[j] = sum;
-                    neighbor[i][j]=false;
+
+        }
+        
+        ans[0] = union;
+        ans[1] = sum;
+        return ans;
+    }
+
+    // 연합국의 인구를 평균으로 바꿔줌
+    public static void  Union(int union, int sum){
+        if(union == 1) return;
+        int avg = sum/union;
+        for(int i =1 ; i<=N ; i++){
+            for(int j = 1; j<=N ; j++){
+                if(isUnion[i][j]){
+                    country[i][j]=avg;
                 }
             }
-
-        }
-
-    }
-
-    // 국경을 여는 나라가 있는지 확인
-    public static boolean check(){
-        boolean flag = false;
-        for(int i = 1 ; i<=num ; i++){
-            int people = country[i];
-            int up=i-N;
-            int down=i+N;
-            int left=i-1;
-            int right=i+1;
-
-            if(up>0 && Math.abs(people-country[up]) >=min && Math.abs(people-country[up])<=max){
-                neighbor[i][up] = true;
-                neighbor[up][i] = true;
-                flag = true;
-            }
-            if(down<=num && Math.abs(people-country[down]) >=min && Math.abs(people-country[down])<=max){
-                neighbor[i][down] = true;
-                neighbor[down][i] = true;
-                flag = true;
-            }
-            if(i%N !=1 && left>0 && Math.abs(people-country[left]) >=min && Math.abs(people-country[left])<=max){
-                neighbor[i][left] = true;
-                neighbor[left][i] = true;
-                flag = true;
-            }
-            if(i%N !=0 && right<=num && Math.abs(people-country[right]) >=min && Math.abs(people-country[right])<=max){
-                neighbor[i][right] = true;
-                neighbor[right][i] = true;
-                flag = true;
-            }
-        }
-        return flag;
-    }
-
-    public static void resetOpen(){
-        for(int i =1 ; i<=num ; i++){
-            for(int j=1 ; j<=num; j++){
-                neighbor[i][j] = false;
-            }
         }
     }
+
 }
