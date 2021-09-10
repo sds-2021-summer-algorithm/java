fbfb099e59190acb3dd191d18d7a484b2693fee1
dls4585 <dls4585@naver.com>
2021-09-10 14:15:04 +0900

    [백인찬] AC

--- solution/BOJ_5430_AC/백인찬.java
+++ solution/BOJ_5430_AC/백인찬.java
@@ -1,12 +1,10 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
     static int T, N;
     static String P;
     static Deque<Integer> deque;
     static boolean mode; // if true : 앞에서부터
-    static List<Integer> answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -17,33 +15,39 @@ public class Main {
             P = br.readLine();
             N = Integer.parseInt(br.readLine());
             deque = new ArrayDeque<>();
-            answer = new ArrayList<>();
             mode = true;
 
             String line = br.readLine();
             line = line.substring(1, line.length() - 1);
             StringTokenizer st = new StringTokenizer(line, ",");
-            while(st.hasMoreTokens()) {
+            for (int i = 0; i < N; i++) {
                 deque.add(Integer.valueOf(st.nextToken()));
             }
 
             int len = P.length();
-            int index = 0;
-            try {
-                while(index < len) {
-                    switch (P.charAt(index++)) {
-                        case 'R':
-                            mode = !mode;
+
+            boolean error = false;
+            for (int i = 0; i < len; i++) {
+                switch (P.charAt(i)) {
+                    case 'R':
+                        mode = !mode;
+                        break;
+                    case 'D':
+                        if(deque.size() == 0) {
+                            error = true;
                             break;
-                        case 'D':
-                            if(mode) {
-                                answer.add(deque.removeFirst());
-                            } else {
-                                answer.add(deque.removeLast());
-                            }
-                            break;
-                    }
+                        }
+                        if(mode) {
+                            deque.removeFirst();
+                        } else {
+                            deque.removeLast();
+                        }
+                        break;
                 }
+            }
+            if(error) {
+                sb.append("error\n");
+            } else {
                 if(deque.isEmpty()) {
                     sb.append("[]\n");
                 }
@@ -61,8 +65,6 @@ public class Main {
                     }
                     sb.append("]\n");
                 }
-            } catch (Exception e) {
-                sb.append("error\n");
             }
         }
         bw.write(sb.toString());
