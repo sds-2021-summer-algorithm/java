9a9a2111d5a628433d38fa32e871e6131cd7bc44
yukyeongmin <75004920+yukyeongmin@users.noreply.github.com>
2021-10-03 14:09:28 +0900

    [유경민] A와 B
    포인터 방식으로 다시 해보았습니다!

--- solution/BOJ_12904_A와-B/ykm.java
+++ solution/BOJ_12904_A와-B/ykm.java
@@ -2,43 +2,46 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+
 
 public class Main{
+    static boolean isReverse = false;
+    static int start;                   // t의 시작 index
+    static int end;                     // t의 마지막 index + 1
     public static void main(String[] args) throws IOException{
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
         String S = br.readLine();
         String T = br.readLine();
+        start = 0;
+        end = T.length();
 
-        System.out.println(change(S,T));
+        System.out.println(check(S,T));
     }
 
-    public static int change(String s, String t) {
-        
-        Queue<String> q = new LinkedList<String>();
-        q.offer(s);
-        while(!q.isEmpty()){
-            String current = q.poll();
+    // T에서 문자를 하나씩 빼나가면서 S가 되는지 확인
+    public static int check(String s, String t) {
+        while(s.length()!=end-start){
+
+            if(isReverse){
+                if(t.charAt(start)=='B'){
+                    isReverse = false;
+                }
+                start++;
+            }else{
+                if(t.charAt(end-1)=='B'){
+                    isReverse = true;
+                }
+                end--;
+            }
             
-            if(current.equals(t)) return 1;
-            if(current.length() >= t.length()) continue;
-
-            String a = addA(current); // 마지막에 A추가
-            if(portion(a,t)) q.offer(a); // target의 일부분이면 q에 추가
-
-            String b = reverse(current); // 뒤집고 마지막에 B추가
-            b = addB(b);
-            if(portion(b,t)) q.offer(b); // target의 일부분이면 q에 추가
         }
-        return 0;
-    }
+        t = t.substring(start, end);
+        if(isReverse) t = reverse(t);
 
-    public static String addA(String s){
-        s = s+"A";
-        return s;
+        if(s.equals(t)) return 1;
+        return 0;
     }
 
     public static String reverse(String s){
@@ -47,16 +50,4 @@ public class Main{
         sb.reverse();
         return sb.toString();
     }
-    public static String addB(String s){
-        s = s+"B";
-        return s;
-    }
-
-    // s1이 s2의 일부인지 확인하자
-    public static boolean portion(String s1, String s2){
-        if(s2.contains(s1)) return true;
-        if(s2.contains(reverse(s1))) return true;
-        return false;
-    }
-
 }
