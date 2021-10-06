0692def86e5da9e0a9e27e0c1ef396d1d448cf05
JW Kim <kl529@naver.com>
2021-10-06 22:22:43 +0900

    [김지원] 12904_A와B (O)
    그리디

---
+++ solution/BOJ_12904_A와-B/김지원.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer s = new StringBuffer(br.readLine());
+        StringBuffer t = new StringBuffer(br.readLine());
+
+        while(s.length() < t.length()) {
+            if(t.charAt(t.length()-1) == 'A') {
+                t.deleteCharAt(t.length()-1);
+            }else if(t.charAt(t.length()-1) == 'B'){
+                t.deleteCharAt(t.length()-1);
+                t.reverse();
+            }
+        }
+
+        if(s.toString().equals(t.toString())) {
+            System.out.println(1);
+        }else {
+            System.out.println(0);
+        }
+    }
+}
