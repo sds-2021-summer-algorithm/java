768707cf806b249810d7230e096eb3cddbf448c3
yukyeongmin <75004920+yukyeongmin@users.noreply.github.com>
2021-11-05 00:18:46 +0900

    [유경민] 최종순위

--- solution/BOJ_3665_최종순위/ykm.java
+++ solution/BOJ_3665_최종순위/ykm.java
@@ -1,87 +1,125 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main{
     static int N;
-    static int[] team; // 작년 순위
-    static int[] rank; // index보다 순위가 높은 팀의 수
-
+    static int[] lastYear; // 작년 순위
+    static boolean[][] thisYear;
+    static int[] indegree;
+    static Queue<Integer> q = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
+
         for(int i = 0 ; i<N; i++){
             int a = Integer.parseInt(br.readLine());
-            team = new int[a + 1];
-            rank = new int[a + 1];
+            lastYear = new int[a + 1];          // 작년 순위
+            thisYear = new boolean[a+1][a+1];   // 올해 순위 저장용 그래프
+            indegree = new int[a + 1];          // 그래프의 진입차수 표시
+
+            boolean isPossible = true;          // IMPOSSIBLE판별저장 (사이클)
+            boolean isCertain = true;           // 위상정렬의 경우의 수가 하나인지 
 
             // 작년 순위 입력받기
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 0 ; j<a; j++){
-                rank[Integer.parseInt(st.nextToken())] = j;
-            }
-
-            // 바뀐 순위 입력받기
-            int b = Integer.parseInt(br.readLine());
-            for(int j = 0 ; j<b; j++){
-                st = new StringTokenizer(br.readLine());
-                int up = Integer.parseInt(st.nextToken());
-                int down = Integer.parseInt(st.nextToken());
-
-                rank[up]--;
-                rank[down]++;
-            }
-
-            if(checkRank()){
-                printRank();
+                lastYear[j+1] = Integer.parseInt(st.nextToken());
+                indegree[lastYear[j+1]] = j;
             }
             
-        }
-    }
+            relectLast();
 
+            int b = Integer.parseInt(br.readLine());
+            // 바뀐 순위 입력받기
+            for(int j = 0 ; j<b; j++){
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
 
-    private static boolean checkRank() {
-        for(int i = 1 ; i<rank.length ; i++){
-            if(rank[i]<0 || rank[i]>=rank.length-1) {
-                System.out.println("IMPOSIBBLE");
-                return false;
-            }else{
-                team[rank[i]] = i;
+                change(x,y);           
             }
-        }
-        return true;
-    }
-
-
-    private static void printRank() {
-        boolean[] check = new boolean[rank.length];
-        boolean flag = true;
-        for(int i = 1; i< rank.length; i++){
-            int current = rank[i];
-
-            if(check[current]){
-                flag = false;
-            }else{
-                check[current] = true;
-            }
-        
-        }
-        if(!flag) {
-            System.out.println("?");
-            return;
-        }
-        else{
+            
             StringBuilder sb = new StringBuilder();
-            for(int j = 0 ; j<team.length-1; j++){
-                sb.append(team[j]);
-                if(j!=team.length-2) sb.append(" ");
+            find(0); // 진입차수가 0인 노드 큐에 넣기
+            for(int j = 1; j<indegree.length; j++){
+                if(q.size() == 0){
+                    isPossible = false;
+                    break;
+                }else if(q.size() > 1){
+                    isCertain = false;
+                    break;
+                }else{
+                    int current = q.poll();
+                    sb.append(current);
+                    if(j != indegree.length-1) sb.append(" ");
+
+                    for(int k = 1; k<indegree.length; k++){
+                        if(thisYear[current][k]){
+                            thisYear[current][k] = false;
+                            if(--indegree[k]==0) q.add(k);
+                        }
+                    }
+                }
             }
-            System.out.println(sb.toString());
+
+            if(!isPossible) {
+                bw.write("IMPOSSIBLE");
+            }else if(!isCertain){
+                bw.write("?");
+            }else{
+                bw.write(sb.toString());
+            }
+
+            if(i!=N-1) bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void change(int x, int y) {
+        if(thisYear[x][y]){
+            thisYear[x][y] = false;
+            thisYear[y][x] = true;
+            indegree[y] --;
+            indegree[x] ++;
+        }else{
+            thisYear[y][x] = false;
+            thisYear[x][y] = true;
+            indegree[x] --;
+            indegree[y] ++;
+        }
+    }
+
+    private static void find(int target) {
+        for(int i = 1; i<indegree.length; i++){
+            if(indegree[i]==target) {
+                q.add(i);
+            }
+        }
+    }
+
+    private static void relectLast() { 
+        //  작년 순위 반영
+        for(int i = 1; i<lastYear.length; i++){
+            int up = lastYear[i];
+
+            for(int j = i+1; j<lastYear.length; j++){
+                int down = lastYear[j];
+                
+                thisYear[up][down] = true;
+            }
         }
     }
 }
