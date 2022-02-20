08a877d813627e723c3d07c0f1a88ba83655a941
Hara Choi <hara77227722@gmail.com>
2022-02-20 13:52:54 +0900

    [최하라] 카드2

---
+++ solution/BOJ_2164_카드2/최하라.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++)
+            deque.addLast(i);
+
+        while (deque.size() > 1) {
+            deque.pollFirst();
+            deque.addLast(deque.pollFirst());
+        }
+
+        System.out.println(deque.pollFirst());
+    }
+}
+
+public class 최하라 {
+
+}
